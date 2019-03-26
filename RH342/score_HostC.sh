306f355a8db1f691bd8f4cb25f1390526788492b
mscjr <m@mscjr.com>
2019-03-26 08:23:34 -0400

    Update score_HostC.sh

--- RH342/score_HostC.sh
+++ RH342/score_HostC.sh
@@ -3,7 +3,7 @@
 # Verify environment
 verify=`cat /usr/bin/polling.sh | grep true | wc -l`
 if [ $verify = "1" ]; then
-	echo -e "\e[32mHostC exam prep appears normal/e[0m"
+	echo -e "\e[32mHostC exam prep appears normal\e[0m"
 fi
 
 # RH342 GRADING SCRIPT
